8d3f81e081214c5fc06afac96938560f860e2533
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-03 22:28:02 +0300

    add step and clicker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Clicker from "components/Clicker";
+import { BrowserRouter, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import 'reset.css'
+
 
 function App() {
   return (
-    <div className="App">
-        <Clicker/>
-    </div>
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Clicker} exact />
+      </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import styles from "./clicker.module.scss";
 
-class Clicker extends Component {
-    render() {
-        return (
-            <div>
-                <h1>jksfjskdjf</h1>
-            </div>
-        );
+const Clicker = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [addNum, setAddNum] = useState(true);
+
+  const stepChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setStepNumber(+value);
+  };
+
+  const addNumber = () => {
+    if (addNum) {
+      setCurrentNumber((oldCurrentNumber) => oldCurrentNumber + stepNumber);
     }
-}
+  };
+
+  return (
+    <div className={styles.clickerContainer}>
+      <div className={styles.numContainer}>
+        <p className={styles.pNumber}>Current Num:</p>
+        <p className={styles.currentNum}> {currentNumber}</p>
+        <p className={styles.pNumber}>Step:</p>
+        <input
+          className={styles.inputStep}
+          type="text"
+          onChange={stepChange}
+        ></input>
+      </div>
+      <button className={styles.btnClick} onClick={addNumber}>
+        Click
+      </button>
+    </div>
+  );
+};
 
 export default Clicker;
